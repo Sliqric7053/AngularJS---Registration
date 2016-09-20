0c39f5447faf201c3cc9d3d6bcbde82b5b340de3
Richard Sithole <richardsit@gmail.com>
2016-09-20 17:42:26 +0200

    Gh pages (#1)
    * Create README.md
    
    * Remove dead code
    
    * Add registration controller
    
    * Update login view
    
    * Update register view

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 var myApp = angular.module('myApp', ['ngRoute']); // [dependant modules go here] --> Occasionally, you'll create a module that depends on the functionality of another module.
 
-// myApp.controller('appController', ['$scope', function($scope) { //$scope is a variable that the app is going to use to pass info along from this controller, into the view or the template.
-//   $scope.message = 'Welcome to my App';
-// }]);
-
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/login', {
--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,3 +1,9 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) {
-  $scope.message = 'Welcome to my App';
+myApp.controller('RegistrationController', ['$scope', function($scope) { ////$scope is a variable that the app is going to use to pass info along from this controller, into the view or the template.
+  $scope.login = function() {
+    $scope.message = 'Welcome ' + $scope.user.email;
+  }
+
+  $scope.register = function() {
+    $scope.message = 'Welcome ' + $scope.user.firstname + ' ' + $scope.user.lastname;
+  }
 }]);
