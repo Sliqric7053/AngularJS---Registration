dc61d831093d17f7db6efad4a21fbe18695f9e94
Sliqric7053 <isloan@ISLOAN.local>
2016-09-21 14:23:59 +0200

    Add Firebase registration controller

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,9 +1,34 @@
-myApp.controller('RegistrationController', ['$scope', function($scope) { ////$scope is a variable that the app is going to use to pass info along from this controller, into the view or the template.
+myApp.controller('RegistrationController',
+  ['$scope', '$firebaseAuth', 'FIREBASE_URL', //$scope is a variable that the app is going to use to pass info along from this controller, into the view or the template.
+  function($scope, $firebaseAuth, FIREBASE_URL) {
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCQQXbU1AkD9GjcwvMgh2BVuEn3OZPMFW4",
+    authDomain: "angular-registration-ab52e.firebaseapp.com",
+    databaseURL: "https://angular-registration-ab52e.firebaseio.com",
+    storageBucket: "angular-registration-ab52e.appspot.com",
+    messagingSenderId: "368084753004"
+  };
+
+  // Initialize SDK
+  firebase.initializeApp(config);
+
+  // add Comment
+  var rootRef = firebase.database().ref();
+
   $scope.login = function() {
     $scope.message = 'Welcome ' + $scope.user.email;
-  }
+  }; // login
 
   $scope.register = function() {
-    $scope.message = 'Welcome ' + $scope.user.firstname + ' ' + $scope.user.lastname;
-  }
-}]);
+    rootRef.$createUser({
+      email: $scope.user.email,
+      password: $scope.user.password
+    }).then(function(regUser) { //JS promise
+      $scope.message = 'Hi ' + $scope.user.firstname + ' ' + $scope.user.lastname + ', thanks for registering!';
+    }).catch(function(error){
+      $scope.message = error.message;
+    }); // createUser
+  } // register
+}]); // factory
