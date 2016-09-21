19fbd6e166ceed42b3ff03a664390ccda43edc7d
Sliqric7053 <isloan@ISLOAN.local>
2016-09-21 14:23:29 +0200

    Add Firebase dependancies

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-var myApp = angular.module('myApp', ['ngRoute']); // [dependant modules go here] --> Occasionally, you'll create a module that depends on the functionality of another module.
+
+
+// [dependant modules go here] --> Occasionally, you'll create a module that depends on the functionality of another module.
+var myApp = angular.module('myApp',
+['ngRoute', 'firebase']) // To use email/password login --> add 'firebase' dependency 
+.constant('FIREBASE_URL', 'https://angular-registration-ab52e.firebaseio.com'); //database URL received from reg Firebase
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
